d5497544514f888ecfa10b50b5fbb445733b653c
lbiasuz <biasuz.l.leonardo@gmail.com>
2023-12-26 22:27:51 -0300

    fix: modificados testes

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -15,11 +15,21 @@ int main(void)
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
 	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(3, *bob);
+
+	delete tmp;
 	delete bob;
 	delete me;
 	delete src;
