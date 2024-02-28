8a81f9209adc6c469845507ddd01d4999dd07855
lbiasuz <biasuz.l.leonardo@gmail.com>
2024-02-28 09:53:38 -0300

    cpp09 ex01 fix

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -25,35 +25,50 @@ bool	RPN::is_valid_input(char *argv) {
 
 int		RPN::calculate(char **argv) {
 	int v1;
+	int v2;
 
 	for (int i = 0; argv[i] != NULL; i++)
 	{
 		if (!this->is_valid_input(argv[i]))
 			throw std::invalid_argument(std::string("Argumento inválido"));
 
-		if (argv[i][0] == '+' && !this->stk.empty()) {
+		if (argv[i][0] == '+' && this->stk.size() > 1) {
 			v1 = this->stk.top();
 			this->stk.pop();
-			this->stk.top() += v1;
+			v2 = this->stk.top();
+			this->stk.pop();
+			v1 += v2;
+			this->stk.push(v1);
 		}
-		else if (argv[i][0] == '-' && !this->stk.empty()) {
+		else if (argv[i][0] == '-' && this->stk.size() > 1) {
 			v1 = this->stk.top();
 			this->stk.pop();
-			this->stk.top() -= v1;
+			v2 = this->stk.top();
+			this->stk.pop();
+			v1 -= v2;
+			this->stk.push(v1);
 		}
-		else if (argv[i][0] == '*' && !this->stk.empty()) {
+		else if (argv[i][0] == '*' && this->stk.size() > 1) {
 			v1 = this->stk.top();
 			this->stk.pop();
-			this->stk.top() *= v1;
+			v2 = this->stk.top();
+			this->stk.pop();
+			v1 *= v2;
+			this->stk.push(v1);
 		}
-		else if (argv[i][0] == '/' && !this->stk.empty())
+		else if (argv[i][0] == '/' && this->stk.size() > 1)
 		{
 			if (this->stk.top() == 0)
 				throw std::runtime_error(std::string("Error: Division by zero;"));
 			v1 = this->stk.top();
 			this->stk.pop();
-			this->stk.top() /= v1;
+			v2 = this->stk.top();
+			this->stk.pop();
+			v1 /= v2;
+			this->stk.push(v1);
 		} else {
+			if (argv[i][0] < 48 || argv[i][0] > 57)
+				throw std::invalid_argument(std::string("Argumento inválido"));
 			this->stk.push(std::strtod(argv[i], NULL));
 		}
 	}
